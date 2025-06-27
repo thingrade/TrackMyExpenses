44beda9dea1d20377cc972c68e048ccb42641af0
thingrade <thingrade@gmail.com>
2025-06-27 14:46:43 +0800

    feat(api): register AppDbContext in Program.cs

--- TrackMyExpenses.API/Program.cs
+++ TrackMyExpenses.API/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TrackMyExpenses.API.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
